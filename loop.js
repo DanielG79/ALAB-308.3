1dd0a232c5b5769c853bdd7fd83005b0c7065ca4
Elucode <125608503+Elulove1@users.noreply.github.com>
2024-05-23 14:15:18 -0400

    Part two completed

--- loop.js
+++ loop.js
@@ -1,14 +1,32 @@
 // Part 1: Fizz Buzz
 
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log("Fizz Buzz")
-    } else if (i % 3 === 0) {
-        console.log("Fizz")
-    } else if (i % 5 === 0) {
-        console.log("BUZZ")
-    } else {
-        console.log(i)
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log("Fizz Buzz")
+//     } else if (i % 3 === 0) {
+//         console.log("Fizz")
+//     } else if (i % 5 === 0) {
+//         console.log("BUZZ")
+//     } else {
+//         console.log(i)
+//     }
+// }
+
+// Part 2: Prime Time
+
+const findNextPrime = (n) => {
+    if (num <= 1) return false
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false
     }
+    return true
 }
 
+let nextPrime = n;
+while (!isPrime(nextPrime)) {
+    nextPrime++;
+    
+}
+console.log(nextPrime);
+
+findNextPrime(4);
